e65902c360a9cdbe58b338c08aa04783a3d88f1f
Igor Ricci Constantino <igorconstantino701@gmail.com>
2022-10-06 10:53:12 -0300

    Update ex029.py

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -3,5 +3,5 @@ velCarro = float(input('Digite a valocidade do carro: '))
 if velCarro > 80:
     print('Você foi multado!')
     print('Sua multa será de R${:.2f}'.format((velCarro - 80) * 7))
-else:
-    print('Tenha um bom dia, dirija com segurança.')
+
+print('Tenha um bom dia, dirija com segurança.')
