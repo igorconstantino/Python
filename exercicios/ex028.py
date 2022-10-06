a070a9c18a94afe4cd7ac508c537041d4dd50a79
Igor Ricci Constantino <igorconstantino701@gmail.com>
2022-10-06 10:48:14 -0300

    att ex

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,8 +1,14 @@
 import random
+from time import sleep
+
 numRand = random.randint(0, 5)
 
 numUser = int(input('Digite seu palpite: '))
+
+print('PROCESSANDO...')
+sleep(2)
+
 if numRand == numUser:
-    print('PARABÉNS!!!')
+    print('PARABÉNS, VOCÊ ACERTOU!!!')
 else:
     print('Não foi dessa vez!!!')
