dcc07586b064c72d8b10ec1656a1ba1f1172d3f9
Igor Ricci Constantino <igorconstantino701@gmail.com>
2023-11-05 17:18:10 -0300

    Update main.py

--- exercicios/db_sqlite/main.py
+++ exercicios/db_sqlite/main.py
@@ -11,12 +11,41 @@ connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
 # SQL
-cursor.execute(f"""
+cursor.execute(f'''
+    DELETE FROM {TABLE_NAME}
+''')
+connection.commit()
+
+cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS {TABLE_NAME}
     (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     weight REAL
     )
-""")
+''')
 connection.commit()
+
+sql = (f'''
+    INSERT INTO {TABLE_NAME} (name, weight) VALUES
+    (?, ?)
+''')
+
+cursor.executemany(
+    sql,
+    (('Igor', 67), ('Larissa', 50), ('Fabiana', 70), ('Robson', 90))
+)
+connection.commit()
+
+
+cursor.execute(f'''
+    SELECT * FROM {TABLE_NAME}
+    WHERE name = "Igor" OR name = "Larissa"
+''')
+
+for row in cursor.fetchall():
+    id_, name, weight = row
+    print(f'id = {id_}, name = {name}, weight = {weight}')
+
+cursor.close()
+connection.close()
